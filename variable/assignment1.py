3c37ef8c3ec7c61cc4012ba71d3008b88242ab52
okaa247 <127107918+okaa247@users.noreply.github.com>
2023-03-20 18:36:14 +0100

    Update assignment1.py

--- variable/assignment1.py
+++ variable/assignment1.py
@@ -2,13 +2,12 @@ current_year = 2023
 average_age = 18
 
 def age_cal():
-    age_calculation = int(input("enter year of birth: "))
     name = input("enter your name: ")
+    age_calculation = int(input("enter year of birth: "))
+    final_age = current_year - age_calculation 
     if age_calculation < 2006:
-        final_age = current_year - age_calculation
         print("hello", name, "your age is", final_age, "years")
     else:
-        final_age = current_year - age_calculation
         print("sorry", name, "you are", final_age, "years old, expected age is 18 and above")
 
 age_cal()        
